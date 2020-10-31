d0d72c2353dc43f22088cf2a9a20ba9250e04b6a
kurai-t <bluekurai0927@hotmail.co.jp>
2020-10-31 17:39:39 +0900

    悩み投稿ページ整理

--- resources/views/test/add.blade.php
+++ resources/views/test/add.blade.php
@@ -1,30 +1,34 @@
 <html>
 @extends('layouts.layout')
 @section('content')
-<h1>悩みを投稿してください</h1>
-
-<body>
-    <form action="/test/add" method="post">
-        <table>
-            @csrf
-            <tr>
-                <th>title: </th>
-                <td><input type="text" name="title"></td>
-            </tr>
-            <tr>
-                <th>main: </th>
-                <td>
-                <textarea rows="10" cols="80" name="main" class="form-control" ></textarea>
-                </td>
-            </tr>
-            <tr>
-                <th>
-                <td><input type="submit" value="send"></td>
-                </th>
-            </tr>
-        </table>
-    </form>
-</body>
+<div class="outer">
+    <div class="inner1">
+        <h1>悩みを記入して送信</h1>
+    </div>
+    <br>
+    <div class="inner2">
+        <form action="/test/add" method="post">
+            <table>
+                @csrf
+                <tr>
+                    <th>title: </th>
+                    <td><input type="text" name="title"></td>
+                </tr>
+                <tr>
+                    <th>main: </th>
+                    <td>
+                        <textarea rows="10" cols="80" name="main" class="form-control"></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                    <td><input type="submit" value="send"></td>
+                    </th>
+                </tr>
+            </table>
+        </form>
+    </div>
+</div>
 @endsection
 
 </html>
